3a8c8b52001c96478bb86915783361a5d97fcec3
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2024-04-14 15:03:51 +0200

    Cleaned main

--- MicroController 1/src/APP/main.c
+++ MicroController 1/src/APP/main.c
@@ -1,22 +1,4 @@
-#include "APP/Display/Display.h"
-#include "HAL/LCD/LCD.h"
-#include "MCAL/RCC/RCC.h"
-#include "Services/Scheduler/Scheduler.h"
-void assert_failed(void)
-{
-    while(1);
-}
 int main(void)
 {
-    RCC_Enable_AHB1_Peripheral(GPIOA);
-    LCD_init(LCD1);
-   // LCD_writeStringAsync(LCD1, "ABC", 3);
-    Display_init();
-    char msg[] = "Hello, world";
-    Display_printAsync(msg, 12);
-    Display_setCursorAsync(1, 0);
-    Display_printCenteredAsync("ABCD", 4);
-    Sched_init();
-    Sched_start();
-    
+
 }
