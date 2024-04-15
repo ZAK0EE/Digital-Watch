470c35f366af72c873cc1cc8c72a5d94e82d4e10
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2024-04-16 00:51:55 +0200

    Cleaned main

--- MicroController 1/src/APP/main.c
+++ MicroController 1/src/APP/main.c
@@ -1,11 +1,7 @@
-// #include "Services/Scheduler/Scheduler.h"
-// void assert_failed (void)
-// {
-//      while(1);
-// }
-// int main(void)
-// {
 
-//      Sched_init();
-//      Sched_start();
-// }
+int main(void)
+{
+
+     Sched_init();
+     Sched_start();
+}
