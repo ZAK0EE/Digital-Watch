373533a45c8232a0e478798aac8179dbf3548586
A-Talaat1 <151054344+A-Talaat1@users.noreply.github.com>
2024-04-16 17:26:38 +0200

    STD Lib

---
+++ MicroController 2/lib/StdTypes.h
@@ -0,0 +1,19 @@
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+/*int*/
+typedef unsigned char u8;
+typedef   signed char s8;
+
+typedef unsigned int u16;
+typedef   signed int s16;
+
+typedef unsigned long u32;
+typedef   signed long s32;
+
+typedef unsigned long long u64;
+typedef signed long long   s64;
+
+#define NULL ((void *)0)
+
+#endif /*STDTYPES_H_*/
