d3f91ebba574fbf6f28f5faae9982567c2bf7bf0
A-Talaat1 <151054344+A-Talaat1@users.noreply.github.com>
2024-04-17 06:40:41 +0200

    Read Switch Fixed and Tested

--- MicroController 2/APP/Read_Switch.c
+++ MicroController 2/APP/Read_Switch.c
@@ -1,7 +1,8 @@
 #include "../src/HAL/Switch/SWITCH.h"
 #include "../lib/StdTypes.h"
+#include "../src/HAL/LED/LED.h"
 
-void CommPublish(u8 Switch);
+void CommPublish(u8 Switch){}
 
 #define INC_SWITCH  0x10
 #define MODE_SWITCH 0x20
@@ -17,31 +18,33 @@ typedef enum
     _Num_Of_Switches,
 }Switches_t;
 
-u8 Current_State [_Num_Of_Switches] = {0};
-u8 Prev_State    [_Num_Of_Switches] = {0};
-u8 State         [_Num_Of_Switches] = {0};  
-u8 Counter       [_Num_Of_Switches] = {0};
+u8 Current_State = 0;
+static u8 Prev_State    [_Num_Of_Switches] = {0};
+static u8 State         [_Num_Of_Switches] = {0};  
+u8 Counter              [_Num_Of_Switches] = {0};
 
 void HSwitch(void)
 {
-    SWITCH_Get_State(INC_Switch , &Current_State[Inc]);
-    SWITCH_Get_State(Mode_Switch, &Current_State[Mode]);
-    SWITCH_Get_State(Edit_Switch, &Current_State[Edit]);
-
+    
     for (u8 iter = 0; iter < _Num_Of_Switches; ++iter)
     {
-        if(Current_State[iter] == Prev_State[iter])
+        SWITCH_Get_State(iter , &Current_State);
+        if(Current_State == Prev_State[iter])
         {
             ++Counter[iter];
         }
-
-        if (Counter[iter] == SAMPLING_NUM)
+        else
         {
-            State[iter] = Current_State[iter];
             Counter[iter] = 0;
         }
+        
 
-        Prev_State[iter] = Current_State[iter];
+        if (Counter[iter] % SAMPLING_NUM == 0)
+        {
+            State[iter] = Current_State;
+        }
+
+        Prev_State[iter] = Current_State;
 
     }
     
@@ -53,18 +56,15 @@ void Read_Switch(void)
     {
         CommPublish(INC_SWITCH);
     }
-    else
+    
     if (State[Mode] == Switch_Pressed)
     {
         CommPublish(MODE_SWITCH);
     }
-    else
+    
     if (State[Edit] == Switch_Pressed)
     {
         CommPublish(Edit_Switch);
     }
-    else
-    {
-        /*Do Nothing*/
-    }
+
 }
