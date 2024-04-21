cd987b6d1bbad164182ba19414d3026f9296b477
Abdelrahman Mamdouh <abdelrahman.mamdouh255@gmail.com>
2024-04-21 22:51:50 +0200

    GPIO Edit

--- MicroController 2/src/MCAL/GPIO/GPIO.h
+++ MicroController 2/src/MCAL/GPIO/GPIO.h
@@ -1,122 +1,126 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
-#include "../../../lib/Error/Error.h" // Include the error handling header file
-#include "../../../lib/StdTypes.h"    // Include the standard types header file
+/* Define standard types for 8-bit unsigned and signed integers */
+typedef unsigned char u8;
+typedef signed char s8;
 
-/* PORTS */
+/* Define standard types for 16-bit unsigned and signed integers */
+typedef unsigned short int u16;
+typedef signed short int s16;
 
-#define GPIO_PORT_A 0x00000000 // Define GPIO port A
-#define GPIO_PORT_B 0x00000001 // Define GPIO port B
-#define GPIO_PORT_C 0x00000002 // Define GPIO port C
+/* Define standard types for 32-bit unsigned and signed integers */
+typedef unsigned int u32;
+typedef signed int s32;
 
-/* PINS */
+/* Define a NULL pointer constant */
+#define NULLPTR ((void *)0)
 
-#define GPIO_PIN_0  0x00000000 // Define GPIO pin 0
-#define GPIO_PIN_1  0x00000001 // Define GPIO pin 1
-#define GPIO_PIN_2  0x00000002 // Define GPIO pin 2
-#define GPIO_PIN_3  0x00000003 // Define GPIO pin 3
-#define GPIO_PIN_4  0x00000004 // Define GPIO pin 4
-#define GPIO_PIN_5  0x00000005 // Define GPIO pin 5
-#define GPIO_PIN_6  0x00000006 // Define GPIO pin 6
-#define GPIO_PIN_7  0x00000007 // Define GPIO pin 7
-#define GPIO_PIN_8  0x00000008 // Define GPIO pin 8
-#define GPIO_PIN_9  0x00000009 // Define GPIO pin 9
-#define GPIO_PIN_10 0x0000000A // Define GPIO pin 10
-#define GPIO_PIN_11 0x0000000B // Define GPIO pin 11
-#define GPIO_PIN_12 0x0000000C // Define GPIO pin 12
-#define GPIO_PIN_13 0x0000000D // Define GPIO pin 13
-#define GPIO_PIN_14 0x0000000E // Define GPIO pin 14
-#define GPIO_PIN_15 0x0000000F // Define GPIO pin 15
+/***********************************************************************/
 
-/* PIN MODES */
+#define GPIO_A ((void*)0x40020000)
+#define GPIO_B ((void*)0x40020400)
+#define GPIO_C ((void*)0x40020800)
+#define GPIO_D ((void*)0x40020C00)
+#define GPIO_E ((void*)0x40021000)
+#define GPIO_H ((void*)0x40021C00)
 
-#define GPIO_CONFG_INPUT_FLOATING 0x00000000 // Define GPIO input floating mode
-#define GPIO_CONFG_INPUT_PU       0x00000001 // Define GPIO input pull-up mode
-#define GPIO_CONFG_INPUT_PD       0x00000002 // Define GPIO input pull-down mode
-#define GPIO_CONFG_ANALOG         0x00000003 // Define GPIO analog mode
-#define GPIO_CONFG_OUTPUT_OD      0x00000004 // Define GPIO output open-drain mode
-#define GPIO_CONFG_OUTPUT_OD_PU   0x00000005 // Define GPIO output open-drain with pull-up mode
-#define GPIO_CONFG_OUTPUT_OD_PD   0x00000006 // Define GPIO output open-drain with pull-down mode
-#define GPIO_CONFG_OUTPUT_PP      0x00000007 // Define GPIO output push-pull mode
-#define GPIO_CONFG_OUTPUT_PP_PU   0x00000008 // Define GPIO output push-pull with pull-up mode
-#define GPIO_CONFG_OUTPUT_PP_PD   0x00000009 // Define GPIO output push-pull with pull-down mode
-#define GPIO_CONFG_AF_OD          0x0000000A // Define GPIO alternative function open-drain mode
-#define GPIO_CONFG_AF_OD_PU       0x0000000B // Define GPIO alternative function open-drain with pull-up mode
-#define GPIO_CONFG_AF_OD_PD       0x0000000C // Define GPIO alternative function open-drain with pull-down mode
-#define GPIO_CONFG_AF_PP          0x0000000D // Define GPIO alternative function push-pull mode
-#define GPIO_CONFG_AF_PP_PU       0x0000000E // Define GPIO alternative function push-pull with pull-up mode
-#define GPIO_CONFG_AF_PP_PD       0x0000000F // Define GPIO alternative function push-pull with pull-down mode
+/***********************************************************************/
 
-/*Alternative Functions*/
-#define GPIO_AF00         0x00000000UL
-#define GPIO_AF01         0x00000001UL
-#define GPIO_AF02         0x00000002UL
-#define GPIO_AF03         0x00000003UL
-#define GPIO_AF04         0x00000004UL
-#define GPIO_AF05         0x00000005UL
-#define GPIO_AF06         0x00000006UL
-#define GPIO_AF07         0x00000007UL
-#define GPIO_AF08         0x00000008UL
-#define GPIO_AF09         0x00000009UL
-#define GPIO_AF10         0x0000000AUL
-#define GPIO_AF11         0x0000000BUL
-#define GPIO_AF12         0x0000000CUL
-#define GPIO_AF13         0x0000000DUL
-#define GPIO_AF14         0x0000000EUL
-#define GPIO_AF15         0x0000000FUL
-#define GPIO_AF_Disabled  0x00000010UL
+#define GPIO_MODE_INPUT_NOPULL                  0x000UL
+#define GPIO_MODE_INPUT_PULLUP                  0x010UL
+#define GPIO_MODE_INPUT_PULLDOWN                0x020UL
+#define GPIO_MODE_INPUT_ANALOG                  0x003UL
+
+#define GPIO_MODE_OUTPUT_PUSHPULL_NOPULL        0x001UL
+#define GPIO_MODE_OUTPUT_PUSHPULL_PULLUP        0x011UL
+#define GPIO_MODE_OUTPUT_PUSHPULL_PULLDOWN      0x021UL
+
+#define GPIO_MODE_OUTPUT_OPENDRAIN_NOPULL       0x101UL
+#define GPIO_MODE_OUTPUT_OPENDRAIN_PULLUP       0x111UL
+#define GPIO_MODE_OUTPUT_OPENDRAIN_PULLDOWN     0x121UL
+
+#define GPIO_MODE_ALTERNATE_PUSHPULL_NOPULL     0x002UL
+#define GPIO_MODE_ALTERNATE_PUSHPULL_PULLUP     0x012UL
+#define GPIO_MODE_ALTERNATE_PUSHPULL_PULLDOWN   0x022UL
+
+#define GPIO_MODE_ALTERNATE_OPENDRAIN_NOPULL    0x102UL
+#define GPIO_MODE_ALTERNATE_OPENDRAIN_PULLUP    0x112UL
+#define GPIO_MODE_ALTERNATE_OPENDRAIN_PULLDOWN  0x122UL
+
+/**********************************************************************/
+
+#define GPIO_PIN0               0x00UL
+#define GPIO_PIN1               0x01UL
+#define GPIO_PIN2               0x02UL
+#define GPIO_PIN3               0x03UL
+#define GPIO_PIN4               0x04UL
+#define GPIO_PIN5               0x05UL
+#define GPIO_PIN6               0x06UL
+#define GPIO_PIN7               0x07UL
+#define GPIO_PIN8               0x08UL
+#define GPIO_PIN9               0x09UL
+#define GPIO_PIN10              0x0AUL
+#define GPIO_PIN11              0x0BUL
+#define GPIO_PIN12              0x0CUL
+#define GPIO_PIN13              0x0DUL
+#define GPIO_PIN14              0x0EUL
+#define GPIO_PIN15              0x0FUL
+
+/**********************************************************************/
+
+#define GPIO_LOW_SPEED             0
+#define GPIO_MEDIUM_SPEED          1
+#define GPIO_HIGH_SPEED            2
+#define GPIO_VERY_HIGH_SPEED       3
+#define GPIO_NumOfAvailableSpeedConfigs 4
+
+/**********************************************************************/
+
+#define GPIO_SET_STATE           0x01UL
+#define GPIO_RESET_STATE         0x00UL
+
+/***********************************************************************/
+
+#define GPIO_AF0                         0x0000UL
+#define GPIO_AF1                         0x0001UL
+#define GPIO_AF2                         0x0002UL
+#define GPIO_AF3                         0x0003UL
+#define GPIO_AF4                         0x0004UL
+#define GPIO_AF5                         0x0005UL
+#define GPIO_AF6                         0x0006UL
+#define GPIO_AF7                         0x0007UL
+#define GPIO_AF8                         0x0008UL
+#define GPIO_AF9                         0x0009UL
+#define GPIO_AF10                        0x000AUL
+#define GPIO_AF11                        0x000BUL
+#define GPIO_AF12                        0x000CUL
+#define GPIO_AF13                        0x000DUL
+#define GPIO_AF14                        0x000EUL
+#define GPIO_AF15                        0x000FUL
+
+/***********************************************************************/
+
+
+typedef enum{
+    GPIO_Ok,
+    GPIO_Nok,
+    GPIO_InvalidParameter,
+    GPIO_NullPtr
+}GPIO_errorState_t;
+
+typedef struct{
+    void* port;
+    u32   pin;
+    u32  mode;
+    u32 speed;
+}GPIO_pinConfig_t;
 
 
 
-/* PIN SPEED */
+GPIO_errorState_t GPIO_initPin(GPIO_pinConfig_t *pinStr);
+GPIO_errorState_t GPIO_setPin(void* port, u32 pin, u32 state);
+GPIO_errorState_t GPIO_getPin(void* port, u32 pin, u32 *pinState);
+GPIO_errorState_t GPIO_configAlterFunc(void* port, u32 pin, u32 alterFunc);
 
-#define GPIO_PIN_LOW_SPEED   0xFFFFFFFF // Define GPIO pin low speed
-#define GPIO_PIN_MID_SPEED   0x00000001 // Define GPIO pin medium speed
-#define GPIO_PIN_HIGH_SPEED  0x00000002 // Define GPIO pin high speed
-#define GPIO_PIN_VHIGH_SPEED 0x00000003 // Define GPIO pin very high speed
-
-/* PIN STATE */
-#define GPIO_PIN_ON  0x00000001UL // Define GPIO pin ON state
-#define GPIO_PIN_OFF 0x00000000UL // Define GPIO pin OFF state
-
-typedef struct
-{
-    u32 port;   // GPIO port
-    u32 pin;    // GPIO pin
-    u32 speed;  // GPIO pin speed
-    u32 mode;   // GPIO pin mode
-    u32 AF;
-} GPIO_Pin_Confg_t; // Structure to hold GPIO pin configuration
-
-/* Function prototypes */
-
-/**
- * @brief Initialize a GPIO pin with the provided configuration.
- * 
- * @param Pin_Config Pointer to the GPIO pin configuration structure.
- * @return Error_t Error code indicating success or failure of the operation.
- */
-Error_t GPIO_Init_Pin(GPIO_Pin_Confg_t *Pin_Config);
-
-/**
- * @brief Set the state of a GPIO pin.
- * 
- * @param GPIO_PORT GPIO port of the pin.
- * @param GPIO_PIN GPIO pin number.
- * @param GPIO_State Desired state of the pin (GPIO_PIN_ON or GPIO_PIN_OFF).
- * @return Error_t Error code indicating success or failure of the operation.
- */
-Error_t GPIO_Set_Pin_State(u32 GPIO_PORT, u32 GPIO_PIN, u32 GPIO_State);
-
-/**
- * @brief Get the state of a GPIO pin.
- * 
- * @param GPIO_PORT GPIO port of the pin.
- * @param GPIO_PIN GPIO pin number.
- * @param Pin_State Pointer to a variable where the pin state will be stored (GPIO_PIN_ON or GPIO_PIN_OFF).
-
-@return Error_t Error code indicating success or failure of the operation.
-*/
-Error_t GPIO_Get_Pin_State(u8 GPIO_PORT, u8 GPIO_PIN, u8 *Pin_State);
-#endif /* GPIO_H_ */
+#endif
